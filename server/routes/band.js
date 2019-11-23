84b2b5734a2f58c73587cb5e79a64928dc262a3d
Pablo <pmotta.dev@gmail.com>
2019-11-22 17:18:50 -0800

    modularized routes, updated server/index.js

--- server/routes/band.js
+++ server/routes/band.js
@@ -1,37 +1,40 @@
 // Requiring our models and passport as we've configured it
 var db = require('../models');
 var passport = require('../config/passport');
+const router = require('express').Router();
 
-module.exports = app => {
-    // @desc -  band signup - creates band name and location
-    // @route - api/band/signup
-    // @access - public
-    // db.band.create
-    //
-    //
-    //
-    // @desc -  user member creation
-    // @route - api/band/usermember
-    // @access - private
-    // Db.member.create
-    // Db.memberinstrument.create
-    // db.bandmember.create
-    //
-    //
-    //
-    // @desc -  additional band member creation - does not have userID
-    // @route - api/band/bandmember
-    // @access - private
-    // Db.member.create;
-    // Db.memberinstrument.create;
-    // db.bandmember.create;
-    //
-    //
-    //
-    // @desc -  additional band member creation - does not have userID
-    // @route - api/band/bandmember
-    // @access - private
-    // Db.member.create
-    // Db.memberinstrument.create
-    // db.bandmember.create
-};
+var isAuthenticatedData = require('../config/middleware/isAuthenticatedData');
+
+// @desc -  band signup - creates band name and location
+// @route - api/band/signup
+// @access - public
+// db.band.create
+//
+//
+//
+// @desc -  user member creation
+// @route - api/band/usermember
+// @access - private
+// Db.member.create
+// Db.memberinstrument.create
+// db.bandmember.create
+//
+//
+//
+// @desc -  additional band member creation - does not have userID
+// @route - api/band/bandmember
+// @access - private
+// Db.member.create;
+// Db.memberinstrument.create;
+// db.bandmember.create;
+//
+//
+//
+// @desc -  additional band member creation - does not have userID
+// @route - api/band/bandmember
+// @access - private
+// Db.member.create
+// Db.memberinstrument.create
+// db.bandmember.create
+
+module.exports = router;
--- server/routes/individuals.js
+++ server/routes/individuals.js
@@ -1,35 +1,36 @@
 // Requiring our models and passport as we've configured it
 var db = require('../models');
 var passport = require('../config/passport');
+const router = require('express').Router();
 
-module.exports = app => {
-    // @desc -  individual signup
-    // @route - api/individual/signup
-    // @access - public
-    // Db.member.create
-    // Db.memberinstrument.create
-    //
-    //
-    //
-    // @desc -  individual login
-    // @route - api/individual/login
-    // @access - public
-    // Db.member.create
-    // Db.memberinstrument.create
-    //
-    //
-    //
-    // @desc -  profile/ edit profile page
-    // @route - api/individual/profile
-    // @access - private
-    // Db.lfg.findAll - where: lfg.memberId === member.id
-    //
-    //
-    //
-    // @desc -  individual update
-    // @route - api/individual/update
-    // @access - private
-    // Db.lfg.findAll - where: lfg.memberId === member.id
-    // Db.member.update - {profilePicture, memberName, location}
-    // Db.memberinstrument.update - {instrument}
-};
+// @desc -  individual signup
+// @route - api/individual/signup
+// @access - public
+// Db.member.create
+// Db.memberinstrument.create
+//
+//
+//
+// @desc -  individual login
+// @route - api/individual/login
+// @access - public
+// Db.member.create
+// Db.memberinstrument.create
+//
+//
+//
+// @desc -  profile/ edit profile page
+// @route - api/individual/profile
+// @access - private
+// Db.lfg.findAll - where: lfg.memberId === member.id
+//
+//
+//
+// @desc -  individual update
+// @route - api/individual/update
+// @access - private
+// Db.lfg.findAll - where: lfg.memberId === member.id
+// Db.member.update - {profilePicture, memberName, location}
+// Db.memberinstrument.update - {instrument}
+
+module.exports = router;
