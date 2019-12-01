a864620ecc016d58b66ccb17004c149a94df0e0c
Pablo <pmotta.dev@gmail.com>
2019-11-30 18:59:57 -0800

    rough prototype of band profile page and its components and styles

---
+++ client/src/components/pages/bandProfile/bandForm/BandForm.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+
+import { useStyles } from './bandForm.style';
+
+export default function BandForm() {
+  const classes = useStyles();
+
+  return (
+    <form noValidate className={classes.form}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={12} alignContent='center'>
+          <TextField
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            variant='standard'
+            margin='normal'
+            id='bandName'
+            label='Band Name'
+            className={classes.nameField}
+            name='bandName'
+            autoComplete='bandName'
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} alignContent='center'>
+          <TextField
+            variant='standard'
+            margin='normal'
+            id='city'
+            label='City'
+            className={classes.addressField}
+            name='city'
+            autoComplete='city'
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} alignContent='center'>
+          <TextField
+            variant='standard'
+            margin='normal'
+            id='state'
+            label='State'
+            className={classes.addressField}
+            name='state'
+            autoComplete='state'
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} alignContent='center'>
+          <TextField
+            variant='standard'
+            margin='normal'
+            id='zipcode'
+            label='Zip Code'
+            className={classes.addressField}
+            name='zipcode'
+            autoComplete='zipcode'
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} alignContent='left' className={classes.floatRight}>
+          <Fab
+            variant='extended'
+            color='primary'
+            aria-label='add'
+            size='large'
+            className={classes.button}
+          >
+            Update
+          </Fab>
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
--- client/src/context/types.js
+++ client/src/context/types.js
@@ -15,5 +15,14 @@ export const LFM_ERROR = 'LFM_ERROR';
 export const SEARCH_ERROR = 'SEARCH_ERROR';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
+//Listings Types
+
+export const LOAD_GENERAL_LISTINGS = 'LOAD_GENERAL_LISTINGS';
+export const SEARCH_LFG = 'SEARCH_LFG';
+export const SEARCH_LFG_ERROR = 'SEARCH_LFG_ERROR';
+export const SEARCH_LFM = 'SEARCH_LFG';
+export const SEARCH_LFM_ERROR = 'SEARCH_LFG_ERROR';
+export const GENERAL_LISTINGS_ERROR = 'GENERAL_LISTINGS_ERROR';
+
 // TO BE REMOVED
 export const TYPE = 'TYPE';
